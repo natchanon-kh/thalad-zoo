d365315461c9a9ce4a020f823c4e4d0c1917ffe3
natchanon.kh <kh.natchanon@gmail.com>
2025-06-23 21:11:20 +0700

    feat: create routing on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Animals from "./pages/Animals";
+import Contact from "./pages/Contact";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <div>
-      <div>APP</div>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/animals" element={<Animals />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
